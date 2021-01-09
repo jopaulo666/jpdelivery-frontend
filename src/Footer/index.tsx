4c6065ed0202f4aca49b451f56174e4a835d001c
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2021-01-09 03:54:38 -0300

    Add Footer

---
+++ src/Footer/index.tsx
@@ -0,0 +1,28 @@
+import './styles.css';
+import {ReactComponent as YoutubeIcon} from './youtube.svg'
+import {ReactComponent as LinkedinIcon} from './linkedin.svg'
+import {ReactComponent as InstagramIcon} from './instagram.svg'
+
+function Footer(){
+    return(
+        <footer className="main-footer">
+            App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+            <div className="footer-icons">
+                <a href="https://www.youtube.com/c/DevSuperior" target="_new">
+                    <YoutubeIcon />
+                </a>
+                <a href="https://www.linkedin.com/in/joãopaulo-damatamendes" target="_new">
+                    <LinkedinIcon />
+                </a>
+                 <a href="https://www.linkedin.com/in/joãopaulo-damatamendes" target="_new">
+                    <InstagramIcon />
+                </a>
+                <a href="https://www.linkedin.com/in/joãopaulo-damatamendes" target="_new">
+
+                </a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
